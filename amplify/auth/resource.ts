8b3a451329bec2d0aaeb51f861b6614ebc2e973d
Peepigans132 <162061062+Peepigans132@users.noreply.github.com>
2025-01-23 14:36:26 -0500

    resource.ts

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -6,21 +6,9 @@ import App from './App';
 import outputs from '../amplify_outputs.json';
 import './index.css';
 import '@aws-amplify/ui-react/styles.css';
-import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
+// Configure Amplify
 Amplify.configure(outputs);
-
-const schema = a.schema({
-  Todo: a.model({
-    content: a.string(),
-  }).authorization((allow) => [allow.owner()]),
-});
-
-export type Schema = ClientSchema<typeof schema>;
-
-export const data = defineData({
-  schema,
-});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
