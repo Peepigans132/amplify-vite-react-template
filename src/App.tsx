24d25c2fc3a1ae59ba483163a58ec963e75ae2dd
Peepigans132 <162061062+Peepigans132@users.noreply.github.com>
2025-01-23 10:34:53 -0500

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-
-const client = generateClient<Schema>();
-
 function App() {
+   // ...
    
   function deleteTodo(id: string) {
     client.models.Todo.delete({ id })
