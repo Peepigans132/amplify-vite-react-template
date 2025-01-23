db64afc49e09396bf0e6b9ed268b5fb0cf192898
Peepigans132 <162061062+Peepigans132@users.noreply.github.com>
2025-01-23 14:03:13 -0500

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,69 @@
-function App() {
-  // ...
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
-  }
 import type { Schema } from '../amplify/data/resource';
 import { useAuthenticator } from '@aws-amplify/ui-react';
 import { useEffect, useState } from 'react';
-import { generateClient } from 'aws-amplify/data';
-
-const client = generateClient<Schema>();
+import { DataStore } from 'aws-amplify';
+import { Todo } from '../models'; // Assuming your model is named `Todo`
 
 function App() {
   const { signOut } = useAuthenticator();
+  const [todos, setTodos] = useState([]);
 
-  // ...
+  // Fetch todos on mount
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const result = await DataStore.query(Todo);
+      setTodos(result);
+    };
+    fetchTodos();
 
-  return (
-    <main>
-      {/* ... */}
-      <button onClick={signOut}>Sign out</button>
-    </main>
-  );
-}
+    // Optional: set up subscription for real-time updates
+    const subscription = DataStore.observe(Todo).subscribe(() => {
+      fetchTodos();
+    });
 
-export default App;
+    return () => subscription.unsubscribe(); // Cleanup subscription on unmount
+  }, []);
 
+  // Create a new todo
+  const createTodo = async () => {
+    await DataStore.save(new Todo({ content: 'New todo item' }));
+    const result = await DataStore.query(Todo);
+    setTodos(result);
+  };
+
+  // Delete a todo
+  const deleteTodo = async (id: string) => {
+    const toDelete = await DataStore.query(Todo, id);
+    if (toDelete) {
+      await DataStore.delete(toDelete);
+      const result = await DataStore.query(Todo);
+      setTodos(result);
+    }
+  };
 
   return (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
-        {todos.map(todo => <li
-          onClick={() => deleteTodo(todo.id)}
-          key={todo.id}>
-          {todo.content}
-        </li>)}
+        {todos.map(todo => (
+          <li
+            onClick={() => deleteTodo(todo.id)}
+            key={todo.id}
+          >
+            {todo.content}
+          </li>
+        ))}
       </ul>
+      <button onClick={signOut}>Sign out</button>
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/">Review next step of this tutorial.</a>
+        <a href="https://docs.amplify.aws/react/start/quickstart/">Review the next step of this tutorial.</a>
       </div>
     </main>
-  )
+  );
 }
+
+export default App;
+
