660acfe7c3df6dae27b767be433ea41a477487df
Peepigans132 <162061062+Peepigans132@users.noreply.github.com>
2025-01-23 10:32:41 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,19 @@ import { generateClient } from "aws-amplify/data";
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+   
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
   }
-
   return (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li
+            onClick={() => deleteTodo(todo.id)}
+            key={todo.id}>{todo.content}</li>
         ))}
       </ul>
       <div>
